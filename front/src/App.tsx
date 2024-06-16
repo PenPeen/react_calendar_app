8fc567f85736fc85b47fa8c86c03067cc55ec400
Pen Peen <ikegami1022.h@gmail.com>
2024-06-16 22:45:19 +0900

    :sparkles: Storeの登録を行うProvidersコンポーネントの定義

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,15 @@
 import { FC } from 'react';
 
 import CalendarBoard from './components/CalendarBoard';
+import Providers from './Providers';
 
 const App: FC = () => {
   return (
     <>
-      <h1>Calendar App</h1>
-      <CalendarBoard />
+      <Providers>
+        <h1>Calendar App</h1>
+        <CalendarBoard />
+      </Providers>
     </>
   );
 };
