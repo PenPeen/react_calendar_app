013f0724fe7f6cbb66caca0d4e708bcb3f5e9be1
Pen Peen <ikegami1022.h@gmail.com>
2024-06-19 15:41:54 +0900

    :heavy_plus_sign: 最新ライセンスをインストールするように変更

--- front/src/Providers.tsx
+++ front/src/Providers.tsx
@@ -1,11 +1,19 @@
 import { FC, PropsWithChildren } from 'react';
 
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Provider as ReduxProvider } from 'react-redux';
 
 import store from './stores';
 
 const Providers: FC<PropsWithChildren> = ({ children }) => {
-  return <ReduxProvider store={store}>{children}</ReduxProvider>;
+  return (
+    <ReduxProvider store={store}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        {children}
+      </LocalizationProvider>
+    </ReduxProvider>
+  );
 };
 
 export default Providers;
