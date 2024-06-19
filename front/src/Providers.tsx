5f8e72a4ae2c00a8bfbc74f95f04d2a419ecce4b
Pen Peen <ikegami1022.h@gmail.com>
2024-06-19 17:03:30 +0900

    :zap: カレンダーの日本語化対応

--- front/src/Providers.tsx
+++ front/src/Providers.tsx
@@ -3,13 +3,14 @@ import { FC, PropsWithChildren } from 'react';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { Provider as ReduxProvider } from 'react-redux';
+import 'dayjs/locale/ja';
 
 import store from './stores';
 
 const Providers: FC<PropsWithChildren> = ({ children }) => {
   return (
     <ReduxProvider store={store}>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ja">
         {children}
       </LocalizationProvider>
     </ReduxProvider>
