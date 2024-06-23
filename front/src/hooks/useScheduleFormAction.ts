e4b27d521493d3bf08cded24ee3623a5f81045da
Pen Peen <ikegami1022.h@gmail.com>
2024-06-23 18:53:19 +0900

    :sparkles: 保存時にDBにデータの永続化を実現

--- front/src/hooks/useScheduleFormAction.ts
+++ front/src/hooks/useScheduleFormAction.ts
@@ -3,9 +3,9 @@ import { useDispatch } from 'react-redux';
 import { scheduleFormSlice } from '@/stores/scheduleForm';
 import { schedulesSlice } from '@/stores/schedules';
 import { Schedule } from '@/types/schedule';
-
+import { post } from '@/utils/api';
+const { setLoading, addSchedule } = schedulesSlice.actions;
 const { openDialog, closeDialog, setValue } = scheduleFormSlice.actions;
-const { addSchedule } = schedulesSlice.actions;
 
 export const useScheduleFormAction = () => {
   const dispatch = useDispatch();
@@ -15,8 +15,13 @@ export const useScheduleFormAction = () => {
   };
   const handleOpenDialog = (day: string) => dispatch(openDialog(day));
   const handleCloseDialog = () => dispatch(closeDialog());
-  const handleStoreForm = (schedule: Schedule) => {
-    dispatch(addSchedule(schedule));
+  const handleStoreForm = async (schedule: Schedule) => {
+    dispatch(setLoading());
+
+    const body = { ...schedule, date: schedule.date };
+    const result = await post('schedules', body);
+
+    dispatch(addSchedule(result));
     dispatch(closeDialog());
   };
 
