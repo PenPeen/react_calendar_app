9b143d63c2ef9796cb963186e769e71b790f66e3
Pen Peen <ikegami1022.h@gmail.com>
2024-06-23 22:16:21 +0900

    :memo: front側のコードに対して、JSDocによる説明を追加した

--- front/src/utils/api.ts
+++ front/src/utils/api.ts
@@ -1,4 +1,4 @@
-import { ScheduleItem } from '@/types';
+import { Schedule, ScheduleItem } from '@/types';
 
 const host = 'http://localhost:8080/api';
 const url = (path: string) => `${host}/${path}`;
@@ -8,26 +8,48 @@ const defaultHeader = {
   },
 };
 
-export const get = async (path: string): Promise<ScheduleItem[]> => {
+/**
+ * エンドポイントからスケジュールの配列を取得する
+ *
+ * @param {string} path - APIエンドポイントへのパス
+ * @returns {Promise<ScheduleItem[]>} スケジュール項目の配列を返すPromiseオブジェクト
+ */
+export const fetchScheduleItemsRequest = async (
+  path: string,
+): Promise<ScheduleItem[]> => {
   const res = await fetch(url(path));
   const result = await res.json();
 
   return result;
 };
 
-export const post = async (
+/**
+ * エンドポイントへスケジュールをPOSTメソッドで送信する
+ *
+ * @param {string} path - APIエンドポイントへのパス
+ * @param {Schedule} body - スケジュール情報
+ * @param {object} [header=defaultHeader] - リクエストのヘッダー（デフォルト: JSON）
+ * @returns {Promise<ScheduleItem>} APIレスポンスを返すPromiseオブジェクト
+ */
+export const storeScheduleRequest = async (
   path: string,
-  body: object,
+  body: Schedule,
   header: object = defaultHeader,
 ) => {
   const options = { ...header, method: 'POST', body: JSON.stringify(body) };
   const res = await fetch(url(path), options);
-  const result = await res.json();
+  const result: ScheduleItem = await res.json();
 
   return result;
 };
 
-export const deleteReq = async (path: string) => {
+/**
+ * エンドポイントへスケジュールの削除リクエストをDELETEメソッドで送信する
+ *
+ * @param {string} path - APIエンドポイントへのパス
+ * @returns {Promise<void>} レスポンスを返さないPromiseオブジェクト
+ */
+export const removeScheduleRequest = async (path: string) => {
   const options = { method: 'DELETE' };
   await fetch(url(path), options);
 
--- front/src/utils/schedule.ts
+++ front/src/utils/schedule.ts
@@ -4,6 +4,13 @@ import { isSameDay } from './calendar';
 
 import { ScheduleItem } from '@/types/schedule';
 
+/**
+ * 指定されたカレンダーの各日付に保存されているスケジュールをマッピングする。
+ *
+ * @param {dayjs.Dayjs[]} calendar - 日付の配列
+ * @param {ScheduleItem[]} schedules - スケジュール一覧
+ * @returns {Object[]} 各日付と関連するスケジュールの配列を持つオブジェクトの配列
+ */
 export const mapSchedulesToDate = (
   calendar: dayjs.Dayjs[],
   schedules: ScheduleItem[],
