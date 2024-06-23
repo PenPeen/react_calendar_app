7e4bc67c79869fcd643d48300202cb2ce9c865b4
Pen Peen <ikegami1022.h@gmail.com>
2024-06-23 17:50:50 +0900

    :sparkles: APIから取得した予定を画面表示するための処理実装

---
+++ front/src/hooks/useFetchSchedule.ts
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import { CalendarState } from '@/stores';
+import { schedulesSlice } from '@/stores/schedules';
+import { get } from '@/utils/api';
+const { setLoading, fetchSchedule } = schedulesSlice.actions;
+
+export const useFetchSchedule = (currentCalendar: CalendarState) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setLoading());
+
+    const fetchAsyncSchedule = async () => {
+      const currentSchedules = await get(
+        `schedules?month=${currentCalendar.month}&year=${currentCalendar.year}`,
+      );
+
+      dispatch(fetchSchedule(currentSchedules));
+    };
+
+    void fetchAsyncSchedule();
+  }, [dispatch, currentCalendar]);
+};
